d4c3d748aaded96bb942c34f659eafb6615fb282
Ezeki Zibzibadze <ez@jok.io>
2022-01-23 10:55:11 +0400

    feat: add PWA support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { IonicModule } from '@ionic/angular'
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './layouts/app/app.component'
+import { AppComponent } from './layouts/app/app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +16,12 @@ import { AppComponent } from './layouts/app/app.component'
     BrowserModule,
     AppRoutingModule,
     IonicModule.forRoot({ mode: 'ios' }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     [
